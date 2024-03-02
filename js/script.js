a327def0ada99842e8f8a76f33a1bac1613baa1c
Abdur Rahim <rahim11500000@gmail.com>
2024-03-02 21:21:11 +0600

    make header section

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-alert("hello world");
+const posts = async()=>{
+    const dataLoad = await fetch("https://openapi.programming-hero.com/api/retro-forum/posts");
+    const data = await dataLoad.json();
+    const postsData = data.posts;
+    console.log(postsData)
+    postsData.forEach(post => {
+    console.log(post)        
+    });
+}
+posts()
